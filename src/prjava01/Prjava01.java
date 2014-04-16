d89ae4c9f6e67587e01b4a834e6c6cad96b35ca1
Alex <Alex@Alex>
2014-04-16 08:29:27 +0200

    Versió 0.1 del projecte prjava01

---
+++ src/prjava01/Prjava01.java
@@ -0,0 +1,36 @@
+/*
+*Tochangethistemplate,chooseTools|Templates
+*andopenthetemplateintheeditor.
+*/
+package prjava01;
+/**
+*
+*@authordaw2_m08uf4pr2
+*/
+import java.awt.Desktop;
+import java.io.*;
+
+public class Prjava01{
+    
+/**
+*@param args the command line arguments
+     * @throws java.io.IOException
+*/
+    public static void main(String[]args)throws IOException{
+            File f = new File("fitxer.html");
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+                    bw.write("<html>");bw.newLine();
+                    bw.write("<head>");bw.newLine();
+                    bw.write("<title>");bw.newLine();
+                    bw.write("Novap&agrave;ginaweb");bw.newLine();
+                    bw.write("</title>");bw.newLine();
+                    bw.write("</head>");bw.newLine();
+                    bw.write("<body>");bw.newLine();
+                    bw.write("Novap&agrave;ginaweb");bw.newLine();
+                    bw.write("</body>");bw.newLine();
+                    bw.write("</html>");bw.newLine();
+                    bw.close();
+            }
+    }
+}
+
